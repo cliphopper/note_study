51cba5faa652d57d1adeadd4ae478dec054a265d
Yukiko Nakamura <ynakamura222@gmail.com>
2022-01-12 22:19:54 +0900

    JSONファイルから検索条件と施設情報を取得

--- pages/result.js
+++ pages/result.js
@@ -16,10 +16,6 @@ export default function Home() {
   //検索結果に表示する施設のid
   const fitem1 = ["f000001","f000002","f000003","f000004","f000005"]
 
-  const itemArea = ["群馬県","栃木県","茨城県","埼玉県","東京都","神奈川県","千葉県"]
-  const itemEmpStatus = ["正職員","契約職員","パート・バイト","業務委託"]
-  const itemCondition = ["介護福祉士","通所介護・デイサービス","ボーナス・賞与あり","社会保険完備","無資格可","年齢不問","車通勤可","夜勤専従あり","グループホーム","訪問介護","未経験可","介護職員初任者研修（旧ヘルパー2級）以上","学歴不問","新規オープン","交通費支給"]
-  
   return (
     <Layout title={title} description={description} username={username} >
       <main className={styles.main}>
@@ -42,19 +38,23 @@ export default function Home() {
                 <tr>
                     <th>都道府県</th>
                     <td>
-                      <SetCheckbox data={itemArea} idname="ia"/>
+                      <SetCheckbox cat="北海道"/>
+                      <SetCheckbox cat="東北"/>
+                      <SetCheckbox cat="関東"/>
                     </td>
                 </tr>
                 <tr>
                     <th>雇用形態</th>
                     <td>
-                      <SetCheckbox data={itemEmpStatus} idname="ie"/>
+                      <SetCheckbox cat="雇用形態"/>
                     </td>
                 </tr>
                 <tr>
                     <th>こだわり条件</th>
                     <td>
-                      <SetCheckbox data={itemCondition} idname="ic"/>
+                      <SetCheckbox cat="人気の条件"/>
+                      <SetCheckbox cat="福利厚生"/>
+                      <SetCheckbox cat="資格"/>
                     </td>
                 </tr>
               </table>
